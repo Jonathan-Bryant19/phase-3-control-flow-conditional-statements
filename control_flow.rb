4619fb3e5abb146a28265750c5b382358a3a21b2
Jonathan Bryant <spectrum19tech@gmail.com>
2022-02-22 19:46:38 -0800

    commit tree 9c1aab228fb627891cdfed69a3a861cf45d16641

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,53 @@
+require 'pry'
+
 def admin_login(username, password)
-  # your code here
+  if username.downcase == "admin" && password == "12345"
+    return "Access granted"
+  else
+    return "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  case 
+  when temperature < 40
+    return "It's brisk out there!"
+  when temperature >= 40 && temperature <= 65
+    return "It's a little chilly out there!"
+  when temperature > 85
+    return "It's too dang hot out there!"
+  else
+    return "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  case
+  when num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+  when num % 3 == 0
+    return "Fizz"
+  when num % 5 == 0
+    return "Buzz"
+  else
+    return num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+" 
+    return num1 + num2
+  when "-"
+    return num1 - num2
+  when "*"
+    return num1 * num2
+  when "/"
+    return num1 / num2
+  else
+    puts "Invalid operation!"
+    return nil
+  end
 end
+
 
